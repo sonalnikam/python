17dd0b4b32084e273989f72beec81426394843ea
SonamVerma9 <sonam.verma@atos.net>
2016-12-30 16:13:35 +0530

    Update settings.py

--- Shift/settings.py
+++ Shift/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '08c85ba2-9efe-4329-85fa-cf029c2d4216'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
