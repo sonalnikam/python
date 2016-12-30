737ac4fb4d277919bafbd5c2fa5e5f2c4b301e6b
SonamVerma9 <sonam.verma@atos.net>
2016-12-30 15:52:33 +0530

    Update settings.py

--- Shift/settings.py
+++ Shift/settings.py
@@ -79,13 +79,12 @@ WSGI_APPLICATION = 'Shift.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': "OpenShiftDb",
-        'USER' : "root",
-        'PASSWORD':"root",
-        'HOST' : "localhost",
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
 
 
 # Password validation
